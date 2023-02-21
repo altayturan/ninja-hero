6ad757cd9abfed9d187118b9c2ff1d4c019c3b06
Altay Turan <69851770+altayturan@users.noreply.github.com>
2023-02-21 10:14:32 +0300

    Refactor: Enemy die connect to event

--- Project-Staj/Assets/Game/Scripts/Enemy/EnemyController.cs
+++ Project-Staj/Assets/Game/Scripts/Enemy/EnemyController.cs
@@ -11,6 +11,8 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private float damage;
     [SerializeField] private Animator animator;
 
+    [SerializeField] private GameEvent EnemyOnDie;
+
     public float Damage
     {
         get
@@ -26,15 +28,6 @@ public class EnemyController : MonoBehaviour
         health *= StatisticManager.Instance.enemyHealthMultiplier;
         animator.SetBool("isRunning", true);
     }
-
-    private void Update()
-    {
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-            //GoldManager.Instance.ChangeGoldWithAmount(30);
-        }
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Player"))
@@ -43,34 +36,15 @@ public class EnemyController : MonoBehaviour
             Destroy(this.gameObject);
         }
 
-        if(collision.collider.TryGetComponent(out BulletController bulletController))
+        if (collision.collider.TryGetComponent(out BulletController bulletController))
         {
             health -= bulletController.BulletDamage;
             if (health <= 0)
             {
-                
+                Destroy(gameObject);
+                EnemyOnDie.Invoke();
             }
         }
-    }
-    #endregion
-
-
-    #region Functions
-
-    public void ChangeHealthWithAmount(float changeAmount)
-    {
-        health += changeAmount;
-    }
-
-    public float GetSpeed()
-    {
-        return speed;
-    }
-
-    public void KillEnemy()
-    {
-        Destroy(gameObject);
-        //GoldManager.Instance.ChangeGoldWithAmount(30);
     }
     #endregion
 }
---
+++ Project-Staj/Assets/Game/Scripts/Enemy/EnemyEventFunctions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEventFunctions : MonoBehaviour
+{
+    public void KillEnemy()
+    {
+        Debug.Log("Girdi"); // Altýn Arttýr
+    }
+}
